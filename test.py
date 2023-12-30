6bbbe92f37bd4124d53ee61d6c26e56137e67f8c
Ahho0927 <poku0927@gmail.com>
2023-12-31 01:47:20 +0900

    update macros

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-from time import sleep
-from data.key import Keys
-from data.VKCODE import *
+from math import sqrt
 
-keys = Keys()
+r1 = int(input("r1 : "))
+r2 = int(input("r2 : "))
+max = int(sqrt(r1**2 + r2**2))
+dot = [0, 0]
+for n in range(max):
+    dot[0] += int((-(n-1)+sqrt(2*r1**2 - (n-1)**2))/2 + ((n-1)+sqrt(2*r1**2 - (n-1)**2))/2)
+    dot[1] += int((-(n-1)+sqrt(2*r2**2 - (n-1)**2))/2 + ((n-1)+sqrt(2*r2**2 - (n-1)**2))/2)
 
-sleep(2)
-keys.PressReleaseKey(VK_SPACE)
+answer = 4*(dot[1] - dot[0]) -4*(r2-r1)
+print(answer)
