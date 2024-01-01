b9e8e43b3bafeacf4dff07ba345b16ee81ec5d7f
Ahho0927 <poku0927@gmail.com>
2024-01-02 00:53:45 +0900

    ui test

---
+++ ui.py
@@ -0,0 +1,10 @@
+from tkinter import Tk
+from tkinter import PhotoImage
+
+tk = Tk()
+tk.title('SORIDAMA')
+tk.iconphoto(False, PhotoImage(file= './data/icon.png'))
+tk.minsize(400, 300)
+tk.
+
+tk.mainloop()
